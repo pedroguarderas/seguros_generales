b81e18c300a17ebcb0472d6f7d309f0002147ff8
pedroguarderas <pedro.felipe.guarderas@gmail.com>
2025-06-02 21:21:49 -0500

    Activo generación del documento para github y pdf.

--- R/01_build.R
+++ R/01_build.R
@@ -104,14 +104,14 @@ file.copy( '~/Development/bibliography/Bibtex/bibliografia_articulos.bib', 'book
 setwd( wd )
 setwd( 'bookdown' )
 outdir <- paste0( wd, 'docs' )
-render_book( input = "lectura_01.Rmd", output_format = "bookdown::gitbook", output_dir = outdir,
-             encoding = "UTF-8", config_file = "_bookdown.yml" )
+# render_book( input = "lectura_01.Rmd", output_format = "bookdown::gitbook", output_dir = outdir,
+#              encoding = "UTF-8", config_file = "_bookdown.yml" )
 
 # render_book( input = "lectura_01.Rmd", output_format = "bookdown::pdf_book", output_dir = outdir,
 #              encoding = "UTF-8", config_file = "_bookdown.yml" )
 
-# render_book( input = "lectura_01.Rmd", output_format = "all", output_dir = outdir,
-#              encoding = "UTF-8", config_file = "_bookdown.yml", clean = FALSE )
+render_book( input = "lectura_01.Rmd", output_format = "all", output_dir = outdir,
+             encoding = "UTF-8", config_file = "_bookdown.yml", clean = FALSE )
 
 wd <- paste0( getActiveProject(), '/' )
 setwd( wd )
